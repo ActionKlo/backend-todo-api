1b607ebb6d7b76c59f05b965f8a16d9cd788ae22
Andrei <actionclaw@gmail.com>
2023-05-06 20:43:49 +0200

    add request for drop table 'tasks'

--- routers/router.go
+++ routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"todoBackedAPI/controllers"
+	"todoBackedAPI/repositories"
 )
 
 func SetupRouters() *gin.Engine {
@@ -11,6 +12,16 @@ func SetupRouters() *gin.Engine {
 	router.GET("/tasks", controllers.GetAllTasks)
 	router.GET("/task/:id", controllers.GetTaskById)
 	router.POST("/task", controllers.CreateTask)
+
+	router.GET("/drop-tasks", func(c *gin.Context) {
+		db, err := repositories.ConnectDB()
+
+		if err != nil {
+			panic(err)
+		}
+
+		db.Migrator().DropTable("tasks")
+	})
 
 	return router
 }
